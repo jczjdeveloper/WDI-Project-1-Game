303217d0ce432320793af146d568adc92bf1d26c
Zhi Jie Justin Cheong <jczjdeveloper@gmail.com>
2017-05-29 15:03:52 +0800

    pre-accelaration

--- game.js
+++ game.js
@@ -1,114 +1,108 @@
 var Game = function() {
 
-  // Game settings
-  var settings = {}; // Containes all game settings
-  settings.spaceshipSpeed = 0; // The speed of the spaceship
-  settings.walls = true; // The spaceship can not go outside the screen (true cannot go out, false can go out)
-  settings.automatic = false; // The spaceship will move by itself
-  settings.godmode = false; // Debug mode
+    // Game settings
+    var settings = {};                     // Containes all game settings
+    settings.spaceshipSpeed = 8;                // The speed of the ball
+    settings.walls = true;                 // The ball can not go outside the screen
+    settings.automatic = false;            // The ball will move by itself
+    settings.godmode = false;              // Debug mode
 
-  // World settings
-  var assets = []; // All game objects
-  var player = new Spaceship(settings); // The player
-  assets[0] = player;
-  var frame = 0; // Frames since the start of the game
+    // World settings
+    var assets = [];                      // All game objects
+    var player = new Spaceship(settings);      // The player
+    assets[0] = player;
+    var frame = 0;                        // Frames since the start of the game
 
-  // Interactions
-  var interactions = {};
-  // interactions.up = false; // Up arrow key pressed
-  // interactions.down = false;            // Down arrow key pressed
-  interactions.left = false; // Left arrow key pressed
-  interactions.right = false; // Right arrow ket pressed
-  interactions.space = false; // Space key pressed
+    // Interactions
+    var interactions = {};
+    interactions.up = false;              // Up arrow key pressed
+    interactions.down = false;            // Down arrow key pressed
+    interactions.left = false;            // Left arrow key pressed
+    interactions.right = false;           // Right arrow ket pressed
+    interactions.space = false;           // Speace key pressed
 
-  // Setup event listeners
-  function setupEvents() {
+    // Setup event listeners
+    function setupEvents() {
 
-    document.addEventListener('keyup', function(event) {
-      var keyName = event.key;
+      document.addEventListener('keyup', function(event){
+        var keyName = event.key;
 
-      switch (keyName) {
-        case "ArrowRight":
-          interactions.right = false;
-          break;
-        case "ArrowLeft":
-          interactions.left = false;
-          break;
-          // case "ArrowUp":                    // don't need
-          //     interactions.up = false;
-          //     break;
-          // case "ArrowDown":                    // don't need
-          //     interactions.down = false;
-          //     break;
-        case "Space":
-          interactions.space = false;
-          break;
-        default:
-          break;
-      }
-    });
+        switch(keyName) {
+          case "ArrowRight":
+              interactions.right = false;
+              break;
+          case "ArrowLeft":
+              interactions.left = false;
+              break;
+          case "ArrowUp":
+              interactions.up = false;
+              break;
+          case "ArrowDown":
+              interactions.down = false;
+              break;
+          default:
+              break;
+        }
+      });
 
-    document.addEventListener('keydown', function(event) {
-      var keyName = event.key;
+      document.addEventListener('keydown', function(event){
+        var keyName = event.key;
 
-      switch (keyName) {
-        case "ArrowRight":
-          interactions.right = true;
-          break;
-        case "ArrowLeft":
-          interactions.left = true;
-          break;
-        // case "ArrowUp":
-        //   interactions.up = true;
-        //   break;
-        // case "ArrowDown":
-        //   interactions.down = true;
-        //   break;
-        case "Space":
-          interactions.space = true;
-          break;
-
-        default:
-          break;
-      }
-    });
+        switch(keyName) {
+          case "ArrowRight":
+              interactions.right = true;
+              break;
+          case "ArrowLeft":
+              interactions.left = true;
+              break;
+          case "ArrowUp":
+              interactions.up = true;
+              break;
+          case "ArrowDown":
+              interactions.down = true;
+              break;
+          default:
+              break;
+        }
+      });
 
 
 
 
-  }
 
-
-
-  // Startup the game
-  function init() {
-    setupEvents();
-  }
-
-  // The render function. It will be called 60/sec
-  function render() {
-
-    for (var i = 0; i < assets.length; i++) {
-      assets[i].render(interactions);
     }
-  }
-
-  window.requestAnimFrame = (function() {
-    return window.requestAnimationFrame ||
-      window.webkitRequestAnimationFrame ||
-      window.mozRequestAnimationFrame ||
-      function(callback) {
-        window.setTimeout(callback, 1000 / 60);
-      };
-  })();
 
 
-  (function animloop() {
-    requestAnimFrame(animloop);
-    render();
-  })();
 
-  init();
+    // Startup the game
+    function init(){
+      setupEvents();
+    }
+
+    // The render function. It will be called 60/sec
+    function render(){
+
+      for(var i=0; i < assets.length; i++){
+        assets[i].render(interactions);
+      }
+    }
+
+    window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       ||
+              window.webkitRequestAnimationFrame ||
+              window.mozRequestAnimationFrame    ||
+              function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+              };
+            })();
+
+
+            (function animloop(){
+              requestAnimFrame(animloop);
+              render();
+            })();
+
+            init();
 }
 
 var g = new Game();
