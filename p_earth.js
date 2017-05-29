908ac5f66a8b78b3f4362906da32faafe92d9315
Zhi Jie Justin Cheong <jczjdeveloper@gmail.com>
2017-05-30 02:03:32 +0800

    added rotating css for earth object

---
+++ p_earth.js
@@ -0,0 +1,35 @@
+var Earth = function(settings) {
+
+  // Settings
+  var earthElement = null;
+  var oxygen = 100;
+  var fuel = 100;
+
+
+
+
+
+
+
+
+
+  function create() {
+    // Create the object asset
+  }
+
+  function init() {
+    // create();
+    earthElement = document.getElementById('earth');
+    earthElement.style.top = '200px';
+    earthElement.style.left = '200px';
+    earthElement.style.height = '300px';
+
+  }
+
+  // this.render = function(interactions) {
+  //   move(interactions);
+  //
+  // }
+
+  init();
+}
