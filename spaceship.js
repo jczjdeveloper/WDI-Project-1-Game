e2feaf99c03132170766166f69930856971e2ca2
Zhi Jie Justin Cheong <jczjdeveloper@gmail.com>
2017-05-29 14:33:18 +0800

    edited

--- spaceship.js
+++ spaceship.js
@@ -1,12 +1,20 @@
 var Spaceship = function(settings) {
 
-    // Settings
-    var spaceshipElement = null;
-    var bullets = [];
+  // Settings
+  var spaceshipElement = null;
+  var spaceshipRotation = 0;
+  var spaceshipVelocity = 0;
+  var friction = 0.9;
+  var yCoord = Math.sin(spaceshipRotation * (180/Math.PI));
+  console.log(yCoord);
+  var xCoord = Math.sin(spaceshipRotation * (180/Math.PI));
+    console.log(xCoord);
+  this.vx = 0;
+  this.vy = 0;
 
 
 
-    /*
+  /*
     bottom:265
     height:100
     left:400
@@ -15,75 +23,66 @@ var Spaceship = function(settings) {
     width:100
 */
 
-    function wall() {
+  function wall() {
 
-      var spaceshipRect = spaceshipElement.getBoundingClientRect();
-      var w = parseInt(window.innerWidth);
-      var h = parseInt(window.innerHeight);
+    var spaceshipRect = spaceshipElement.getBoundingClientRect();
+    var w = parseInt(window.innerWidth);
+    var h = parseInt(window.innerHeight);
 
-      if(spaceshipRect.bottom > h){
-        spaceshipElement.style.top = (h-spaceshipRect.height) + 'px';
-      }
-
-      if(spaceshipRect.top < 0){
-        spaceshipElement.style.top = '0px';
-      }
-
-      if(spaceshipRect.left < 0){
-          spaceshipElement.style.left = '0px';
-      }
-
-      if(spaceshipRect.right > w){
-          spaceshipElement.style.left = ( w - spaceshipRect.width) + 'px' ;
-      }
-
-
-
-
-
+    if (spaceshipRect.bottom > h) {
+      spaceshipElement.style.top = (h - spaceshipRect.height) + 'px';
     }
 
-    // Move the spaceship based on arrowkey
-    function move(interactions){
-
-      if(interactions.up){
-        spaceshipElement.style.top = parseInt(spaceshipElement.style.top)-8+"px";
-      }
-
-      if(interactions.down){
-        spaceshipElement.style.top = parseInt(spaceshipElement.style.top)+8+"px";
-      }
-
-      if(interactions.left){
-        spaceshipElement.style.left = parseInt(spaceshipElement.style.left)-8+"px";
-      }
-
-      if(interactions.right){
-        spaceshipElement.style.left = parseInt(spaceshipElement.style.left)+8+"px";
-      }
-
-      if(settings.walls){
-        wall();
-      }
+    if (spaceshipRect.top < 0) {
+      spaceshipElement.style.top = '0px';
     }
 
-
-    function create() {
-        // Create the object asset
+    if (spaceshipRect.left < 0) {
+      spaceshipElement.style.left = '0px';
     }
 
-    function init(){
-      // create();
-      spaceshipElement = document.getElementById('spaceship');
-      spaceshipElement.style.top = '400px';
-      spaceshipElement.style.left = '300px';
-      spaceshipElement.style.height = '100px';
-
+    if (spaceshipRect.right > w) {
+      spaceshipElement.style.left = (w - spaceshipRect.width) + 'px';
     }
 
-    this.render = function(interactions){
-      move(interactions);
+  }
+
+  // Move the spaceship based on arrowkey
+  function move(interactions) {
+
+    if (interactions.left) {
+      spaceshipElement.style.transform = 'rotate(' + (parseInt(spaceshipRotation) - 8) + 'deg)';
+      yCoord = spaceshipRotation - 8;
+      console.log(yCoord);
     }
 
-    init();
+    if (interactions.right) {
+      spaceshipElement.style.transform = 'rotate(' + (parseInt(spaceshipRotation) + 8) + 'deg)';
+      xCoord = spaceshipRotation + 8;
+      console.log(xCoord);
+    }
+
+          // thrusting without friction
+    function thrusting() {
+
+      if (interactions.space) {
+      vx += xCoord * this.thrust;
+      vy += yCoord * this.thrust;
+      // with friction
+      vx *= friction;
+      vy *= friction;
+      // velocity with friction applied to x and y coords
+      this.x = this.vx;
+      this.y = this.vy;
+    }
+  }
+
+    if (settings.walls) {
+      wall();
+    }
+  }
+
+// converting radians to degrees
+function toDegree(angle) {
+
 }
