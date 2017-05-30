acbe39c63d16301643e4dae4a5b4806f39457433
Zhi Jie Justin Cheong <jczjdeveloper@gmail.com>
2017-05-30 21:27:19 +0800

    added clipart spaceship, spaceship moving

--- game.js
+++ game.js
@@ -1,173 +1,122 @@
 var Game = function() {
 
-  // Game settings
-  var settings = {}; // Containes all game settings
-  settings.spaceshipSpeed = 8; // The speed of the ball
-  settings.walls = true; // The ball can not go outside the screen
-  settings.automatic = false; // The ball will move by itself
-  settings.godmode = false; // Debug mode
-  settings.time = 60;
+    // Game settings
+    var settings = {};                     // Containes all game settings
+    settings.walls = false;                 // The spaceship can not go outside the screen
+    settings.automatic = false;            // The spaceship will move by itself
+    settings.acceleration = 0.3;          // Acceleration per frame
+    settings.maxspeed = 5;                 // Max speed of the Spaceship
+    settings.turnspeed = 4;                // How fast will the Spaceship turn
 
-  // World settings
-  var assets = []; // All game objects
-  var player = new Spaceship(settings); // The player
-  assets[0] = player;
-  var frame = 0; // Frames since the start of the game
+    // World settings
+    var assets = [];                      // All game objects
+    var player = new Spaceship(settings);      // The player
+    assets[0] = player;
+    var frame = 0;                        // Frames since the start of the game
 
-  // Interactions
-  var interactions = {};
-  // interactions.up = false; // Up arrow key pressed
-  // interactions.down = false; // Down arrow key pressed
-  interactions.up = false;              // Up arrow key pressed
-  interactions.down = false;            // Down arrow key pressed
-  interactions.left = false; // Left arrow key pressed
-  interactions.right = false; // Right arrow ket pressed
-  interactions.space = false; // Space key pressed
+    // Interactions
+    var interactions = {};
+    // // interactions.up = false;              // Up arrow key pressed
+    // interactions.down = false;            // Down arrow key pressed
+    interactions.left = false;            // Left arrow key pressed
+    interactions.right = false;           // Right arrow ket pressed
+    interactions.space = false;           // Space key pressed
+    interactions.accelerate = false;      // a key pressed
+    interactions.break = false;           // z key pressed
 
-  // Setup event listeners
-  function setupEvents() {
+    // Setup event listeners
+    function setupEvents() {
 
+      document.addEventListener('keyup', function(event){
+        var keyName = event.key;
 
+        switch(keyName) {
+          case "ArrowRight":
+              interactions.right = false;
+              break;
+          case "ArrowLeft":
+              interactions.left = false;
+              break;
+          // case "ArrowUp":
+          //     interactions.up = false;
+          //     break;
+          // case "ArrowDown":
+          //     interactions.down = false;
+          //     break;
+          case "a":
+              interactions.accelerate = false;
+              break;
+          case "z":
+              interactions.break = false;
+              break;
+          default:
+              break;
+        }
+      });
 
-// SPACESHIP EVENT listeners
-    document.addEventListener('keyup', function(event) {
-      var keyName = event.key;
+      document.addEventListener('keydown', function(event){
+        var keyName = event.key;
 
-      switch(keyName) {
-        case "ArrowRight":
-            interactions.right = false;
-            break;
-        case "ArrowLeft":
-            interactions.left = false;
-            break;
-        case "ArrowUp":
-            interactions.up = false;
-            break;
-        case "ArrowDown":
-            interactions.down = false;
-            break;
-        default:
-            break;
-      }
-
-
-
-
-
-      // SPACESHIP
-      // var keyName = event.key;
-      // var keyCode = event.code;
-      // switch (keyName) {
-      //   case "ArrowRight":
-      //     interactions.right = false;
-      //     break;
-      //   case "ArrowLeft":
-      //     interactions.left = false;
-      //     break;
-      //   default:
-      //     break;
-      // }
-      // switch (keyCode) {
-      //   case "Space":
-      //     interactions.space = false;
-      //     break;
-      //   default:
-      //     break;
-      // }
-    });
-
-    document.addEventListener('keydown', function(event) {
-      var keyName = event.key;
-
-      switch(keyName) {
-        case "ArrowRight":
-            interactions.right = true;
-            break;
-        case "ArrowLeft":
-            interactions.left = true;
-            break;
-        case "ArrowUp":
-            interactions.up = true;
-            break;
-        case "ArrowDown":
-            interactions.down = true;
-            break;
-        default:
-            break;
-      }
+        switch(keyName) {
+          case "ArrowRight":
+              interactions.right = true;
+              break;
+          case "ArrowLeft":
+              interactions.left = true;
+              break;
+          // case "ArrowUp":
+          //     interactions.up = true;
+          //     break;
+          // case "ArrowDown":
+          //     interactions.down = true;
+          //     break;
+          case "a":
+              interactions.accelerate = true;
+              console.log('pressed');
+              break;
+          case "z":
+              interactions.break = true;
+              break;
+          default:
+              break;
+        }
+      });
 
 
-
-
-      // SPACESHIP
-      // var keyName = event.key;
-      // var keyCode = event.code;
-      // switch (keyName) {
-      //   case "ArrowRight":
-      //     interactions.right = true;
-      //     break;
-      //   case "ArrowLeft":
-      //     interactions.left = true;
-      //     break;
-      //   default:
-      //     break;
-      // }
-      // switch (keyCode) {
-      //   case "Space":
-      //     interactions.space = true;
-      //     break;
-      //   default:
-      //     break;
-      // }
-    });
-
-
-
-
-
-  }
-
-
-
-  // Startup the game
-  function init() {
-    setupEvents();
-  }
-
-  // The render function. It will be called 60/sec
-  this.render = function() {
-
-    for (var i = 0; i < assets.length; i++) {
-      assets[i].render(interactions);
     }
-//     // seconds per frame
-// secondsPassed = frame / 60;
-// // reiteration
-// frame ++;
-// // time passed function
-// // timer(settings.time, seondsPassed);
-
-  }
 
 
 
-var self = this;
-  window.requestAnimFrame = (function() {
-    return window.requestAnimationFrame ||
-      window.webkitRequestAnimationFrame ||
-      window.mozRequestAnimationFrame ||
-      function(callback) {
-        window.setTimeout(callback, 1000 / 60);
-      };
-  })();
+    // Startup the game
+    function init(){
+      setupEvents();
+    }
+
+    // The render function. It will be called 60/sec
+    this.render = function(){
+
+      for(var i=0; i < assets.length; i++){
+        assets[i].render(interactions);
+      }
+    }
+
+    var self = this; // Add this line
+    window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       ||
+              window.webkitRequestAnimationFrame ||
+              window.mozRequestAnimationFrame    ||
+              function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+              };
+            })();
 
 
-  (function animloop() {
-    requestAnimFrame(animloop);
-    self.render();
-  })();
+            (function animloop(){
+              requestAnimFrame(animloop);
+              self.render();
+            })();
 
-  init();
+            init();
 }
 
 var g = new Game();
--- spaceship.js
+++ spaceship.js
@@ -1,176 +1,129 @@
-var Spaceship = function(settings) {
+var Spaceship = function(settings){
 
-  // Settings
-  var spaceshipElement = null;
-  var spaceshipRotation = 0;
-  var velY = 0;
-  var velX = 0;
+  var shipElement = null;
+  var angle = 0;
+  var speed = 0;
 
+  var shipPosition = {};
 
-  // Resources
-  var oxygen = 100;
-  var fuel = 100;
-  var ore = 0;
+  function turn(interactions){
 
-
-  // Thrust velocity
-  var thrust = 0.8;
-
-
-
-
-  /*
-    bottom:265
-    height:100
-    left:400
-    right:500
-    top:165
-    width:100
-*/
-
-  function wall() {
-
-    var spaceshipRect = spaceshipElement.getBoundingClientRect();
-
-    var w = parseInt(window.innerWidth);
-    var h = parseInt(window.innerHeight);
-
-    if (spaceshipRect.bottom > h) {
-      spaceshipElement.style.top = (h - spaceshipRect.height) + 'px';
+    if(interactions.left){
+      angle -= settings.turnspeed;
     }
 
-    if (spaceshipRect.top < 0) {
-      spaceshipElement.style.top = '0px';
+    if(interactions.right){
+      angle += settings.turnspeed;
     }
 
-    if (spaceshipRect.left < 0) {
-      spaceshipElement.style.left = '0px';
+    if(angle > 360){
+      angle =0;
     }
 
-    if (spaceshipRect.right > w) {
-      spaceshipElement.style.left = (w - spaceshipRect.width) + 'px';
-    }
-
-
-  }
-
-
-  // Move the spaceship around manually
-  function move(interactions) {
-    if (interactions.up) {
-      spaceshipElement.style.top = parseInt(spaceshipElement.style.top) - 8 + "px";
-    }
-
-    if (interactions.down) {
-      spaceshipElement.style.top = parseInt(spaceshipElement.style.top) + 8 + "px";
-    }
-
-    if (interactions.left) {
-      spaceshipElement.style.left = parseInt(spaceshipElement.style.left) - 8 + "px";
-    }
-
-    if (interactions.right) {
-      spaceshipElement.style.left = parseInt(spaceshipElement.style.left) + 8 + "px";
-    }
-
-    // SPACESHIP
-    // if (interactions.left) {
-    //   spaceshipElement.style.transform = 'rotate(' + (parseInt(spaceshipRotation) - 8) + 'deg)';
-    //   spaceshipRotation = spaceshipRotation - 8;
-    //   console.log(spaceshipRotation);
-    // }
-    //
-    // if (interactions.right) {
-    //   spaceshipElement.style.transform = 'rotate(' + (parseInt(spaceshipRotation) + 8) + 'deg)';
-    //   spaceshipRotation = spaceshipRotation + 8;
-    //   console.log(spaceshipRotation);
-    // }
-    //
-    // if (interactions.space) {
-    //   // convert angle to radians (direction that spaceship is facing)
-    //   var radians = spaceshipRotation / Math.PI * 180;
-    //   // current X and Y coord of spaceship
-    //   function getOffset(spaceship) {
-    //     spaceship = spaceship.getBoundingClientRect();
-    //     return {
-    //       left: spaceship.left + window.scrollX,
-    //       top: spaceship.top + window.scrollY
-    //     }
-    //   }
-    //   // Change in X and Y coords
-    //   velY += Math.sin(radians) * thrust;
-    //   velX += Math.cos(radians) * thrust;
-    //   // Apply friction factor (deceleration)
-    //   velY *= 0.9;
-    //   velX *= 0.9;
-    //   // New change in coords with friction applied
-    //   getOffset(spaceship).top -= velY;
-    //   getOffset(spaceship).left -= velX;
-    //   console.log('velocityY is ' + velY + 'velocityX is ' + velX);
-    // }
-
-
-
-
-    if (settings.walls) {
-      wall();
+    if(angle < -360){
+      angle =0;
     }
   }
 
+  function accelerate(interactions){
 
+    if(speed > 0){
+      speed -= settings.acceleration/4;
 
-
-  function spaceshipplanetcollision() {
-    var spaceshipRect = spaceshipElement.getBoundingClientRect();
-    var earth = document.getElementById('earth').getBoundingClientRect();
-    // CIRCULATE COLLISION DETECTION BETWEEN SPACESHIPS AND OTHER PLANETS.
-
-    var spaceshipCol = {
-      radius: spaceshipRect.width/2,
-      x: spaceshipRect.left,
-      y: spaceshipRect.top,
-    };
-    var earthCol = {
-      radius: earth.width/2,
-      x: earth.left,
-      y: earth.top,
-    };
-
-    console.log(spaceshipCol.radius, spaceshipCol.x, spaceshipCol.y)
-    var dx = spaceshipCol.x - earthCol.x;
-    var dy = spaceshipCol.y - earthCol.y;
-    var distance = Math.sqrt(dx * dx + dy * dy);
-    if (distance < spaceshipCol.radius + earthCol.radius) {
-      console.log("Collision with Earth detected!");
-      // collision detected!
+      if(speed < 0){
+        speed =0;
+      }
     }
-  };
-  // SHIP RESOURCES
-  function resources(contact) {
 
-    // if contact value = 1 {
+    if(speed < 0){
+      speed += settings.acceleration/4;
 
+      if(speed > 0){
+        speed =0;
+      }
+    }
 
+    if(interactions.accelerate){
+      speed += settings.acceleration;
+    }
+
+    if(interactions.break){
+      speed -= settings.acceleration;
+    }
+
+    // Friction factor
+    if(speed > settings.maxspeed){
+      speed = settings.maxspeed;
+    }
+
+    // Friction factor
+    if(speed < (settings.maxspeed*-0.9)){
+      speed = settings.maxspeed*-0.9;
+    }
   }
 
-  function create() {
-    // Create the object asset
+  function move(){
+
+    // Set speed
+    shipPosition.x += Math.cos(toRadians(angle)) * speed;
+    shipPosition.y += Math.sin(toRadians(angle)) * speed;
+    shipElement.style.top = shipPosition.y  + "px";
+    shipElement.style.left =  shipPosition.x + "px";
+
+    // Set angle
+    shipElement.style.transform = 'rotate('+ (angle+90) + 'deg)';
   }
 
-  function init() {
-    // create();
-    spaceshipElement = document.getElementById('spaceship');
-    spaceshipElement.style.top = '400px';
-    spaceshipElement.style.left = '400px';
-    spaceshipElement.style.height = '100px';
-
+  this.render = function(interactions){
+      accelerate(interactions);
+      turn(interactions);
+      move();
   }
 
-  this.render = function(interactions) {
-    move(interactions);
-    spaceshipplanetcollision();
+  function toRadians (angle) {
+    return angle * (Math.PI / 180);
+  }
 
+  function init(){
+    shipElement = document.getElementById('ship');
+
+    var shipElementRect = shipElement.getBoundingClientRect();
+    shipPosition = {
+      y: shipElementRect.top,
+      x: shipElementRect.left
+    }
+
+    shipElement.style.transform = 'rotate('+ angle + 'deg)';
   }
 
   init();
 }
+
+
+
+// function spaceshipplanetcollision() {
+//   var spaceshipRect = spaceshipElement.getBoundingClientRect();
+//   var earth = document.getElementById('earth').getBoundingClientRect();
+//
+//   // CIRCULAR COLLISION DETECTION BETWEEN SPACESHIPS AND OTHER PLANETS.
+//
+//   var spaceshipCol = {
+//     radius: spaceshipRect.width / 2,
+//     x: spaceshipRect.left,
+//     y: spaceshipRect.top,
+//   };
+//   var earthCol = {
+//     radius: earth.width / 2,
+//     x: earth.left,
+//     y: earth.top,
+//   };
+//
+//   // console.log(spaceshipCol.radius, spaceshipCol.x, spaceshipCol.y)
+//   var dx = spaceshipCol.x - earthCol.x;
+//   var dy = spaceshipCol.y - earthCol.y;
+//   var distance = Math.sqrt(dx * dx + dy * dy);
+//   if (distance < spaceshipCol.radius + earthCol.radius) {
+//     console.log("Collision with Earth detected!");
+//     // collision detected!
+//   }
+// };
